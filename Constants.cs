a3c5c53f3170fee5195cc54e0b7cba2b150c1d9f
Måns Andersson <mail@mansandersson.se>
2012-12-19 23:07:31 +0100

    Programming constants (e.g. max 32 bit integer)

--- Constants.cs
+++ Constants.cs
@@ -29,9 +29,25 @@ namespace Calculator
         public Constants()
         {
             this.ConstantDict = new Dictionary<string, double?>();
+
+            // Mathematical constants
             this.ConstantDict.Add("pi", 3.141592653589793); // Pi
             this.ConstantDict.Add("e", 2.718281828);        // Natural Logarithm
             this.ConstantDict.Add("phi", 1.61803398874);    // The Golden Ratio
+
+            // Programming constants
+            this.ConstantDict.Add("s32max", Int32.MaxValue);   // 32 bit integer max
+            this.ConstantDict.Add("s32min", Int32.MinValue);   // 32 bit integer min
+            this.ConstantDict.Add("u32max", UInt32.MaxValue);  // 32 bit unsigned integer max
+            this.ConstantDict.Add("u32min", UInt32.MinValue);  // 32 bit unsigned integer min
+            this.ConstantDict.Add("s16max", Int16.MaxValue);   // 16 bit integer max
+            this.ConstantDict.Add("s16min", Int16.MinValue);   // 16 bit integer min
+            this.ConstantDict.Add("u16max", UInt16.MaxValue);  // 16 bit unsigned integer max
+            this.ConstantDict.Add("u16min", UInt16.MinValue);  // 16 bit unsigned integer min
+            this.ConstantDict.Add("s8max", 127);               // 8 bit integer max
+            this.ConstantDict.Add("s8min", -127);              // 8 bit integer min
+            this.ConstantDict.Add("u8max", 255);               // 8 bit unsigned integer max
+            this.ConstantDict.Add("u8min", 0);                 // 8 bit unsigned integer min
         }
         /// <summary>
         /// checks if a string is declared in the the dictionary of constants
