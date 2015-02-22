1137df9d80e16fc5f6d347535c3df9955a79c2eb
Måns Andersson <mail@mansandersson.se>
2015-02-22 17:47:07 +0100

    Group output of hex and binary by 4 characters.

---
+++ NoSpaceCopyTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public class NoSpaceCopyTextBox : System.Windows.Forms.TextBox
+    {
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+
+                case 0x301: //WM_COPY
+                    {
+                        Clipboard.SetText(this.Text.Replace(" ", ""));
+                        return;
+                    }
+
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
